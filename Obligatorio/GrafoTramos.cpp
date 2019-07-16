a5fd46b237bb46e48d5493d6e029a444c4443272
mathiaspouso18 <mathiaspouso18@gmail.com>
2019-07-15 21:17:53 -0300

    Se agrega dfs
    Se agrega dfs para posteriormente trabajar con tramos y paradas.

--- Obligatorio/GrafoTramos.cpp
+++ Obligatorio/GrafoTramos.cpp
@@ -9,7 +9,7 @@ void CrearGrafo (Grafo &G)
     }
 }
 
-//Precondición: la arista no pertenece al grafo.
+//PrecondiciÃ³n: la arista no pertenece al grafo.
 void InsertarAristaGrafo(Grafo &g, Tramo t){
     InsFrontListaAdy(g[IdOrigenTramo(t)],IdDestinoTramo(t));
     if (IdOrigenTramo(t)!= IdDestinoTramo(t)){
@@ -17,7 +17,7 @@ void InsertarAristaGrafo(Grafo &g, Tramo t){
     }
 }
 
-//Precondición: el vértice pertenece al grafo.
+//PrecondiciÃ³n: el vÃ©rtice pertenece al grafo.
 int gradoVertice(Grafo g, int vertice){
      int cont = LargoListaAdy(g[vertice]);
      if (!PerteneceListaAdy(g[vertice],vertice)){
@@ -33,4 +33,34 @@ Boolean PerteneceAristaGrafo (Grafo g, Ciudad ciudad1, Ciudad ciudad2){
 
 Boolean PerteneceVerticeGrafo(Grafo g, int vertice){
     return PerteneceListaAdy(g[vertice], vertice);
+}
+
+Boolean ExisteTramoEntreCiudades(Grafo G, int u, int v, Boolean &Existe)
+{
+    Existe = FALSE;
+    Boolean visitado[M];
+    for (int i=0; i<M; i++)
+        visitado[i] = FALSE;
+    DFS(G, u, v, visitado, Existe);
+    return Existe;
+}
+
+void DFS(Grafo G, int actual, int v, Boolean visitado[M], Boolean &Existe)
+{
+    visitado[actual] = TRUE;
+    if (actual == v)
+        Existe = TRUE;
+    else
+    {
+        ListaAdy aux = G[actual];
+        int j=0;
+        while ((aux != NULL) && (!visitado[v]))
+        {
+            if (!visitado[aux->vert])
+                DFS(G, aux->vert, v, visitado, Existe);
+            aux = aux->sig;
+        }
+        if (visitado[v])
+            Existe = TRUE;
+    }
 }
